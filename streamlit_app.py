2863e22987fe2db98e3b4b78da4b6b6f304b3347
jjkomosi <jjkomosi@syr.edu>
2026-02-16 06:01:29 +0000

    hw4

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,10 @@ st.set_page_config(page_title="HW Manager", page_icon="📚")
 hw1_page = st.Page("HW/HW1.py", title="HW 1", icon="📄")
 hw2_page = st.Page("HW/HW2.py", title="HW 2", icon="🌐")
 hw3_page = st.Page("HW/HW3.py", title="HW 3", icon="🐈")
+hw4_page = st.Page("HW/HW4.py", title="HW 4", icon="🎒")
 
 # Create navigation
-nav = st.navigation([hw1_page, hw2_page, hw3_page])
+nav = st.navigation([hw1_page, hw2_page, hw3_page, hw4_page])
 
 # Run the selected page
 nav.run()
